ba271b5d7fff33596362df9ee338f67fa5f6850b
Michael Humphries <michaelhumphries1997@gmail.com>
2019-02-27 08:46:16 +0000

    modified pong.py

--- pong.py
+++ pong.py
@@ -1,10 +1,32 @@
 import gym
-import numpy
+import numpy as np
 
 def main():
     env = gym.make("Pong-v0")
     observation = env.reset() # gets the image
 
+    # hyperparameters
+    episode_number = 0
+    batch_size = 10
+    gamma = 0.99
+    decay_rate = 0.99
+    num_hidden_layer_neurons = 200
+    input_dimensions = 80 * 80
+    learning_rate = 1e-4
+
+    episode_number = 0
+    reward_sum = 0
+    running_reward = None
+    prev_processed_observations = None
+
+    weights = {
+        '1': np.random.randn(num_hidden_layer_neurons, input_dimensions) / np.sqrt(input_dimensions),
+        '2': np.random.randn(num_hidden_layer_neurons) / np.sqrt(num_hidden_layer_neurons)
+
+    }
+    
 
 main()
+
+
 
